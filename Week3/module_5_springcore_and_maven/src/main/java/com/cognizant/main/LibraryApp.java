8d42b1bf04b0e2058b467482aa2ed50711e10434
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2026-07-01 19:49:45 +0530

    Week3Mod5_Ex1_CofigureApplication_SpringCore_And_Maven

---
+++ Week3/module_5_springcore_and_maven/src/main/java/com/cognizant/main/LibraryApp.java
@@ -0,0 +1,11 @@
+package com.cognizant.main;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");}
+}
+
