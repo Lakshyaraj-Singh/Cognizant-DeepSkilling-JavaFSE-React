d6f45b7d7abb39b241810640bdff4adf272a16d8
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2026-07-11 22:27:34 +0530

    Week4Ex4_RESTCountryWebService Done

--- Week4/springrest_springboot/src/main/java/com/cognizant/springrest/service/CountryService.java
+++ Week4/springrest_springboot/src/main/java/com/cognizant/springrest/service/CountryService.java
@@ -2,9 +2,9 @@ package com.cognizant.springrest.service;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.stereotype.Service;
 import com.cognizant.springrest.model.Country;
-
+@Service
 public class CountryService {
 
     public Country getCountry() {
